ea7f4224b6fdd17af177e3968a75f08297e14a16
Tomas <tomasmedero92@gmail.com>
2023-09-20 14:02:44 -0300

    updated

--- src/helpers/getData.ts
+++
@@ -1,9 +0,0 @@
-import { datas } from '../data/datas'
-import { TitleInfo } from '../types/types'
-
-export const getData = (titletype: string): TitleInfo[] => {
-  return datas.filter(
-    (data: TitleInfo) =>
-      data.programType === titletype && data.releaseYear >= 2010
-  )
-}
