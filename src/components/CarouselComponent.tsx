a51a92eba507eb175d1e7dd45735a457243f4ccd
Tomas <tomasmedero92@gmail.com>
2024-03-20 05:54:17 -0300

    .

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAPITrending } from "../helpers";
 import { TitleInfo } from "../types/types";
+import { Link } from "react-router-dom";
 
 export const CarouselComponent: React.FC = () => {
 
@@ -27,29 +28,32 @@ export const CarouselComponent: React.FC = () => {
         <>
             <h2 className="text-2xl font-bold mb-4">Todas las Tendencias</h2>
             <div className="flex overflow-x-auto space-x-4 p-4 border border-gray-300 rounded-md shadow-md">
-                {titles.map(({ id, posterUrl, name, rating, programType }) => (
+                {titles.map(({ id, posterUrl, name, rating, programType, media_type }) => (
                     <div key={id} className="flex-shrink-0 w-48 min-w-[200px]">
-                        <div className="relative" style={{ height: '300px' }}>
-                            <img
-                                src={posterUrl}
-                                alt={name}
-                                className="w-full h-full object-cover rounded-md"
-                            />
-                            {rating && (
-                                <div className='absolute top-2 right-2 bg-green-300 font-bold rounded-xl p-1'>
-                                    {rating}
-                                </div>
-                            )}
-                        </div>
-                        <p className="text-center mt-2">{name}</p>
-                        <div className='text-base text-gray-400 text-center '>
-                            {programType && (
-                                <p>
-                                    {programType.charAt(0).toUpperCase() +
-                                        programType.slice(1)}
-                                </p>
-                            )}
-                        </div>
+                        <Link to={`/card/${media_type}/${id}`}>
+                            <div className="relative" style={{ height: '300px' }}>
+                                <img
+                                    src={posterUrl}
+                                    alt={name}
+                                    className="w-full h-full object-cover rounded-md"
+                                />
+                                {rating && (
+                                    <div className='absolute top-2 right-2 bg-green-300 font-bold rounded-xl p-1'>
+                                        {rating}
+                                    </div>
+                                )}
+                            </div>
+                            <p className="text-center mt-2">{name}</p>
+                            <div className='text-base text-gray-400 text-center '>
+                                {programType && (
+                                    <p>
+                                        {programType.charAt(0).toUpperCase() +
+                                            programType.slice(1)}
+                                    </p>
+                                )}
+                            </div>
+                        </Link >
+
                     </div>
                 ))}
             </div>
